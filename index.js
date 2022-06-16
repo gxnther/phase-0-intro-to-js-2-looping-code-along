e75fcb2f12095bdeb76550bd99a4610a4a958c7a
Gunther <gunt.valentine@gmail.com>
2022-06-16 17:55:26 -0400

    commit tree 39887a74c66a37c54d33bbf1911bb6a2b0ee681d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+
+// function writeCards(surprise, event) {
+//     let newSurprise = [];
+//     for (let i = 0; i < surprise.length; i++) {
+//         newSurprise.push(`Thank you, ${surprise[i]}, for the wonderful ${event} gift!`); 
+//     }
+//     console.log(newSurprise);
+//     return newSurprise;
+// }
+// writeCards(surprise);
+
+// function countDown() {
+//     let countDown = 10;
+//     while (countDown > -1) {
+//         console.log(countDown--)
+//     }
+// }
+// countDown;
+
+function writeCards(name, event){
+    let singleName = [];
+    for (let i = 0; i < name.length; i++) {
+        singleName.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+        return singleName;
+        // console.log(`Thank you, ${singleName}, for the wonderful ${event} gift!`)         
+}                                  
+
+function countDown(number){
+    while (number >= -1){
+        console.log(number--)
+    }
+}
